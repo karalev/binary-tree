08eb176cc7641547cb907393b1819586e11fdb7d
Karaliou Kiryl <karalev.kirill@gmail.com>
2016-01-30 17:15:02 +0300

    add insert + constructor

--- binary-tree.js
+++ binary-tree.js
@@ -2,11 +2,40 @@
 
 class BinaryTree {
 	constructor() {
-
+        this._root = null;
 	}
 
 	insert(data) {
+        var root = this.root;
 
+        if(!root){
+            this.root = new Node(data);
+            return;
+        }
+
+        var currentNode = root;
+        var newNode = new Node(data); 
+
+        while(currentNode){
+            if(data < currentNode.data){
+                if(!currentNode.left){
+                    currentNode.left = newNode;
+                    break;
+                }
+                else{
+                    currentNode = currentNode.left;
+                }
+            }
+            else{
+                if(!currentNode.right){
+                    currentNode.right = newNode;
+                    break;
+                }
+                else{
+                    currentNode = currentNode.right;
+                }
+            }
+        }
 	}
 
 	contains(data) {
