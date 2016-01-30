b769603d1f3edcc6b8bcc45796b72774d6440ab5
Karaliou Kiryl <karalev.kirill@gmail.com>
2016-01-30 17:39:51 +0300

    add contains and support function along with sum explanations

--- binary-tree.js
+++ binary-tree.js
@@ -2,12 +2,12 @@
 
 class BinaryTree {
 	constructor() {
-        this._root = null;
+        this.root = null;
 	}
 
 	insert(data) {
         var root = this.root;
-
+        //case when there's no root
         if(!root){
             this.root = new Node(data);
             return;
@@ -15,7 +15,7 @@ class BinaryTree {
 
         var currentNode = root;
         var newNode = new Node(data); 
-
+        //if the value is less than the current node's
         while(currentNode){
             if(data < currentNode.data){
                 if(!currentNode.left){
@@ -26,6 +26,7 @@ class BinaryTree {
                     currentNode = currentNode.left;
                 }
             }
+            //if the value is greater than the current node's
             else{
                 if(!currentNode.right){
                     currentNode.right = newNode;
@@ -39,15 +40,58 @@ class BinaryTree {
 	}
 
 	contains(data) {
+        var found       = false;
+        var current     = this.root
 
+        //make sure there's a node to search
+        while(!found && current){
+
+            //if the value is less than the current node's, go left
+            if (data < current.data){
+                current = current.left;
+
+            //if the value is greater than the current node's, go right
+            } else if (data > current.data){
+                current = current.right;
+
+            //values are equal
+            } else {
+                found = true;
+            }
+        }
+
+        // true if the node was found
+        return found;
 	}
 
 	remove(data) {
-
+        
 	}
+    traverse(process){
+        function inOrder(node){
+            if (node){
+
+                //traverse the left subtree
+                if (node.left !== null){
+                    inOrder(node.left);
+                }            
+
+                //call the process method on this node
+                process.call(this, node);
+
+                //traverse the right subtree
+                if (node.right !== null){
+                    inOrder(node.right);
+                }
+            }
+        }
+
+        //start with the root
+        inOrder(this._root);
+    }
 
 	size() {
-
+        
 	}
 
 	isEmpty() {
